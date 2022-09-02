4cd75a2536bc141fdd4a5a4fb37a8760eca6286e
kadirdundar <100041382+kadirdundar@users.noreply.github.com>
2022-09-02 12:19:36 +0300

    Initial Commit

---
+++ fotografpaylasmauygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fotografpaylasmauygulamasi
+//
+//  Created by Kadir Dündar on 2.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
