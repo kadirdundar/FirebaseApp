7b7331e2bcf59aab2f5e0fa222b1da22fdfabfb3
kadirdundar <100041382+kadirdundar@users.noreply.github.com>
2022-09-05 14:42:33 +0300

    classlar_olusturuldu

--- fotografpaylasmauygulamasi/ViewController.swift
+++ fotografpaylasmauygulamasi/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var sifreTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func girisYapTiklandi(_ sender: Any) {
+    }
+    
+    @IBAction func kayitOlTiklandi(_ sender: Any) {
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+    }
 }
 
