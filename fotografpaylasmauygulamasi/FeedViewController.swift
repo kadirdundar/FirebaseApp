933d5d41181ed135c734eaf72e8ed35088dae552
kadirdundar <100041382+kadirdundar@users.noreply.github.com>
2022-09-20 17:53:36 +0300

    hatalar giderildi

--- fotografpaylasmauygulamasi/FeedViewController.swift
+++ fotografpaylasmauygulamasi/FeedViewController.swift
@@ -6,17 +6,65 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
     @IBOutlet weak var tableView: UITableView!
+    var yorumdizisi = [String]()
+    var gorseldizisi = [String]()
+    var emaildizisi = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
+        firebaseverilerial()
     }
     
-
-
+    func firebaseverilerial(){
+        
+        let firestoredatabase = Firestore.firestore()
+        firestoredatabase.collection("Post").addSnapshotListener{(snapshot, error) in
+            if error != nil {
+                print(error?.localizedDescription)
+                
+            }
+            else {
+                if snapshot?.isEmpty != true  &&  snapshot != nil{
+                    for document in snapshot!.documents{
+                        
+                        if let gorselURL = document.get("gorselurl") as? String{
+                            self.gorseldizisi.append(gorselURL)
+                                                    
+                        }
+                        if let yorum = document.get("yorum") as? String{
+                            self.yorumdizisi.append(yorum)
+                        }
+                        if let email = document.get("email") as? String{
+                            self.emaildizisi.append(email)
+                        }
+                        
+                    }
+                    self.tableView.reloadData()
+                }
+            }
+        }
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emaildizisi.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! feedCellTableViewCell
+        cell.emailText.text = emaildizisi[indexPath.row]
+        cell.kullaniciyorum.text = yorumdizisi[indexPath.row]
+        cell.postImage.image = UIImage(named: "Drawing")
+        
+        
+        return cell
+    }
 
 }
--- fotografpaylasmauygulamasi/feedCellTableViewCell.swift
+++ fotografpaylasmauygulamasi/feedCellTableViewCell.swift
@@ -6,14 +6,13 @@
 //
 
 import UIKit
-import FirebaseFirestore
 
-class feedCellTableViewCell: UITableViewCell, UITableViewDelegate, UITableViewDataSource {
+class feedCellTableViewCell: UITableViewCell {
    
     
-
+    @IBOutlet weak var kullaniciyorum: UILabel!
+    
     @IBOutlet weak var emailText: UILabel!
-    @IBOutlet weak var kullanıcıYorum: UIView!
     @IBOutlet weak var postImage: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -23,39 +22,12 @@ class feedCellTableViewCell: UITableViewCell, UITableViewDelegate, UITableViewDa
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
             
-        
-        tableView.delegate = self
-        tableView.dataSource = self
+       
         // Configure the view for the selected state
     }
     
-    func firebaseverilerial(){
-        
-        let firestoredatabase = Firestore.firestore()
-        firestoredatabase.collection("Post").addSnapshotListener{(snapshot, error) in
-            if error != nil {
-                print(error?.localizedDescription)
-                
-            }
-            else {
-                if snapshot?.isEmpty != true  &&  snapshot != nil{
-                    for document in snapshot!.documents{
-                        let documantID = document.documentID
-                        print(documantID)
-                    }
-                }
-            }
-        }
-        
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! feedCellTableViewCell
-        return cell
-    }
+ 
+
 }
 
 
